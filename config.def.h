71998eb67c6352b97b55e1fbbdc3b69956f32b91
yogeshdcool <dyogesh.official@gmail.com>
2022-05-28 16:36:53 +0530

    patched colorbar

--- config.def.h
+++ config.def.h
@@ -25,6 +25,10 @@ static char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+	[SchemeStatus]  = { normfgcolor, normbgcolor,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { selfgcolor, selbgcolor,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}+    [SchemeTagsNorm]  = { normfgcolor, normbgcolor,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { selfgcolor, selbgcolor,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { normfgcolor, normbgcolor,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 	[SchemeScratchSel]  = { selfgcolor, selbgcolor,  col_red  },
 	[SchemeScratchNorm] = { selfgcolor, selbgcolor,  col_orange },
 };
