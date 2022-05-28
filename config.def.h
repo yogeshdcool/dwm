de7c28dcef2256c872dd818c1090259cd2fe99fe
yogeshdcool <dyogesh.official@gmail.com>
2022-05-28 16:27:06 +0530

    patched xrdb

--- config.def.h
+++ config.def.h
@@ -13,19 +13,20 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_red[]         = "#FF0000";
-static const char col_orange[]      = "#FF8800";
-static const char *colors[][3]      = {
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char col_red[]         = "#FF0000";
+static char col_orange[]      = "#FF8800";
+static char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeScratchSel]  = { col_gray4, col_cyan,  col_red  },
-	[SchemeScratchNorm] = { col_gray4, col_cyan,  col_orange },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+	[SchemeScratchSel]  = { selfgcolor, selbgcolor,  col_red  },
+	[SchemeScratchNorm] = { selfgcolor, selbgcolor,  col_orange },
 };
 
 /* tagging */
@@ -140,6 +141,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
